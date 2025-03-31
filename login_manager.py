81941099e961085b9f3110c9b6384119cd87dc86
Sam Patel <sampatel379@gmail.com>
2025-03-31 12:39:10 +0100

    Added the check registration method

--- login_manager.py
+++ login_manager.py
@@ -10,3 +10,8 @@ class LoginManager():
     def __init__(self, master_file_path : str):
         self.master_file_path = master_file_path
         self.hasher = HashManager()
+    
+    
+    def check_registration(self) -> bool:
+        """Checks if a user has already registered and returns True or False."""
+        return os.path.exists(self.master_file_path)
