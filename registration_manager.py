9638db494b43ca7f7699aa6b82f58ddf4d40cd32
Sam Patel <sampatel379@gmail.com>
2025-03-31 12:47:24 +0100

    Added the save master password method.

--- registration_manager.py
+++ registration_manager.py
@@ -13,3 +13,14 @@ class RegistrationManager:
     def check_registration(self) -> bool:
         """Checks if a user has already registered and returns True or False."""
         return os.path.exists(self.master_file_path)
+    
+
+    def save_master_password(self, hashed_password : str) -> bool:
+        """Saves the hashed master password to a file and returns True if successful, otherwise False."""
+        user_data = {"master_password" : hashed_password}
+        try:
+            with open(self.master_file_path, mode = "w") as file:
+                json.dump(user_data, file)
+                return True
+        except Exception:
+            return False
