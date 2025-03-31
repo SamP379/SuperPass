18f0183de3ce508efb110273aaac2a5c5eb88033
Sam Patel <sampatel379@gmail.com>
2025-03-31 12:41:04 +0100

    Added the check login password method

--- login_manager.py
+++ login_manager.py
@@ -11,7 +11,17 @@ class LoginManager():
         self.master_file_path = master_file_path
         self.hasher = HashManager()
     
-    
+
     def check_registration(self) -> bool:
         """Checks if a user has already registered and returns True or False."""
         return os.path.exists(self.master_file_path)
+    
+
+    def check_login_password(self, login_password : str) -> bool:
+        """Handles verifying a login password against the stored master password."""
+        try:
+            with open(self.master_file_path, mode = "r") as file:
+                master_password = json.load(file).get("master_password")
+                return self.hasher.verify_password(login_password, master_password)
+        except Exception:
+            return False
