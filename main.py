aafb17b4a92c2c54b6861e314c18f40d7be80593
Sam Patel <sampatel379@gmail.com>
2025-04-01 13:55:03 +0100

    Added resource path function to main file

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import os
+import sys
 from superpass import SuperPass
 
+LOGO_IMAGE_NAME = "logo.png"
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
 def main():
-    logo_file_path = "images/logo.png"
+    logo_file_path = resource_path(LOGO_IMAGE_NAME)
     superpass = SuperPass(logo_file_path)
 
 if __name__ == "__main__":
